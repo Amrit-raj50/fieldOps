51c184efe77e7680b1ba96e1a7b322737c8deb37
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-08-25 19:18:32 +0530

    initialized backend with MVC folder structure

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send("server is running!");
+})
+
+app.listen(3000 , () => {
+    console.log("server is runnning on the post 3000");
+})

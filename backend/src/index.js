8e624d218050f64792f4027ea5d693818ac47fbc
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-08-25 20:02:17 +0530

    database connection established

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
-
+const connectDB = require('./config/db.js');
 const express = require('express');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
+connectDB();
 app.get('/',(req,res) => {
     res.send("server is running!");
 })

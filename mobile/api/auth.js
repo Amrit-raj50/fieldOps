561b2765cab1df62dbc8feda6651018d17d9c91b
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-08-28 02:27:36 +0530

    signup page is fully working from frontend to backend

---
+++ mobile/api/auth.js
@@ -0,0 +1,38 @@
+import { API_URI } from "./config";
+console.log(API_URI);
+
+export const userRegister = async(userData) => {
+    try{
+        console.log("Sending request to : " , API_URI);
+
+        const response = await fetch(
+            `${API_URI}api/user/register`,
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body:JSON.stringify(userData),
+            }
+        );
+
+        console.log(userData);
+        console.log(response)
+
+        const data = response.json();
+        console.log(data);
+
+        console.log("backend response :" , data);
+
+        if(!response.ok){
+            throw new Error(
+                data.message || "registration failed"
+            )
+        }
+
+        return data;
+    }catch(error){
+        console.log("registration failed with :",error);
+        throw error;
+    }
+}
---
+++ mobile/app/(tabs)/login.jsx
@@ -0,0 +1,8 @@
+import {View , Text , TextInput , Button} from 'react-native',
+export default function Login(){
+    return(
+        <View>
+            <Text>Log in</Text>
+        </View>
+    )
+}

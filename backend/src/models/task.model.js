7a118c1141440cb001d4691a6a3658242aba01c6
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-09-02 12:56:43 +0530

    added accept field in the task schema

--- backend/src/models/task.model.js
+++ backend/src/models/task.model.js
@@ -2,62 +2,66 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema(
     {
-        title :{
-            type:String,
-            required:[true, "Task title required."],
-            trim : true,
-            maxLength:[120 , "title cannot exceed 120 characters."]
+        title: {
+            type: String,
+            required: [true, "Task title required."],
+            trim: true,
+            maxLength: [120, "title cannot exceed 120 characters."]
         },
-        description :{
-            type:String,
-            trim:true,
-            default : '',
-            maxLength : [2000 , "description cannot exceeds 2000 characters."]
+        description: {
+            type: String,
+            trim: true,
+            default: '',
+            maxLength: [2000, "description cannot exceeds 2000 characters."]
         },
-        employee:{
-            type:String,
-            required:[true,"Employee is required"],
-            trim:true,
+        employee: {
+            type: String,
+            required: [true, "Employee is required"],
+            trim: true,
         },
-        priority:{
-            type:String,
-            enum:['Low','Medium','High'],
-            required : [true,"priority is required."],
-            default:'Medium',
+        priority: {
+            type: String,
+            enum: ['Low', 'Medium', 'High'],
+            required: [true, "priority is required."],
+            default: 'Medium',
         },
-        location : {
-            type : String,
-            required : [true,"Location is srequired."],
-            trim:true
+        location: {
+            type: String,
+            required: [true, "Location is srequired."],
+            trim: true
         },
-        dueDate :{
-            type:Date,
-            reuired:[true,'Due date is required.'],
+        dueDate: {
+            type: Date,
+            reuired: [true, 'Due date is required.'],
             // validate : {
             //     validator:function (value){
             //         return value >= new Date().setHours(0,0,0,0);
             //     },
-                // message : "due date cannot be in the past",
+            // message : "due date cannot be in the past",
             // },
         },
-        status :{
-            type:String,
-            enum:["Pending","In Progress" , "Completed","Cancled"],
-            default:"pending",
+        status: {
+            type: String,
+            enum: ["Pending", "In Progress", "Completed", "Cancled"],
+            default: "pending",
         },
-        empId : {
-            type:String,
-            default:null
+        empId: {
+            type: String,
+            default: null
         },
-        evidence:{
-            type:String,
-            default:null
+        evidence: {
+            type: String,
+            default: null
+        },
+        accept: {
+            type: Boolean,
+            default: false
         }
     },
     {
-        timestamps:true,
+        timestamps: true,
     }
 );
 
-const task = mongoose.model('Task',taskSchema);
+const task = mongoose.model('Task', taskSchema);
 module.exports = task;

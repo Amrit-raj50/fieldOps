047cbbc60f7bcc2dc34c79283f938eea46387e51
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-09-04 13:57:55 +0530

    post complain from client side to the database

---
+++ mobile/api/complainCreation.js
@@ -0,0 +1,29 @@
+import {API_URI} from '../api/config';
+
+export const complainCreation = async(taskData) => {
+    try{
+        const response = await fetch(
+            `${API_URI}api/user/admin/create-complain`,
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body:JSON.stringify(taskData),
+            }
+        )
+
+        const data = response.json();
+
+        if(!response.ok){
+            throw new Error(
+                data.message || "Creation Failed"
+            )
+        }
+
+        return data;
+    }catch(error){
+        console.log("Creation Failed.",error.message);
+        throw error;
+    }
+}

861358701fd8f14b5357f1dc1611ff8c112f2e2f
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-09-04 13:31:46 +0530

    client section is created along with role based login for client

---
+++ mobile/app/clients/(tabs)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Tabs.Screen
+                name="dashboard"
+                options={{ title: "Dashboard" }}
+            />
+        </Tabs>
+    );
+}
